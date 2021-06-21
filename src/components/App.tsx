1dd8b3e4d7f33883732e4bb58199891c6f03ab3f
Lei Jin <leijin.illinois@gmail.com>
2021-06-21 16:18:40 -0500

    Initial commit of starter code

---
+++ src/components/App.tsx
@@ -0,0 +1,23 @@
+import './../assets/scss/App.scss'
+
+import React from 'react'
+
+import {hot} from 'react-hot-loader'
+
+const reactLogo = require('./../assets/img/react_logo.svg')
+
+class App extends React.Component<Record<string, unknown>, undefined> {
+  public render() {
+    return (
+      <div className="app">
+        <h1>Hello World!</h1>
+        <p>Foo to the bar</p>
+        <img src={reactLogo.default} height="480" />
+      </div>
+    )
+  }
+}
+
+declare let module: Record<string, unknown>
+
+export default hot(module)(App)
