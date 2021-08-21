96bb905369435f8f6202c5dba7fdc849b92fe201
Lei Jin <leijin.illinois@gmail.com>
2021-08-21 12:48:55 -0500

    Upkeep

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,4 @@ import './assets/scss/App.scss'
 import App from './components/App'
 import {render} from 'react-dom'
 
-render(<App />, document.getElementById('optimatAPP'))
+render(<App />, document.getElementById('myAPP'))
